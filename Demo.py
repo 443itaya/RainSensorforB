069e679dfbdc94e994fa21ab3e9e1137fea0f55c
443itaya <yoshimi.itaya@gmail.com>
2019-02-26 15:29:43 +0900

    ver.1

--- Demo.py
+++ Demo.py
@@ -3,31 +3,39 @@ import cv2
 import PIL.Image, PIL.ImageTk
 import numpy as np
 
+
 class App:
     def __init__(self,window,window_title,video_source=0):
         self.window = window
         self.window.title(window_title)
     
-        self.window.configure(bg='black')
+        self.window.configure(bg='green')
         self.video_source = video_source
 
         self.vid = MyVideoCapture(self.video_source)
         self.state = False
-        
-        self.stream = tkinter.Canvas(window, width = 950, height = 720)
-        self.stream.grid(row=0, rowspan=2, column=0, padx=35, pady=150)
 
-        self.rain = tkinter.Canvas(window, width = 792, height = 500)
-        self.rain.grid(row=0, column=1, padx=25, pady=220)
+        self.background = tkinter.Canvas(window, width = 1920, height = 1080)
+        self.background.place(x=0, y=0)
+        self.img_background = PIL.ImageTk.PhotoImage(PIL.Image.open('background.png').convert('RGB'))
+        self.background.create_image(0, 0, image = self.img_background, anchor = tkinter.NW)
+
+        self.stream = tkinter.Canvas(window, width = 950, height = 720)
+        self.stream.grid(row=0, rowspan=2, column=0, padx=55, pady=150)
+
+        self.rain = tkinter.Canvas(window, width = 794, height = 500)
+        self.rain.grid(row=1, column=1, padx=0, pady=220)
 
         self.btn = tkinter.Button(window, text='START', font=("", 40))
-        self.btn.grid(row=0, column=1, padx=300, pady=30, sticky=tkinter.S+tkinter.W+tkinter.E)
+        self.btn.grid(row=1, column=1, padx=300, pady=80, sticky=tkinter.S+tkinter.W+tkinter.E)
 
 
         def StartStop(event):
             if self.state == False:
                 self.state = not self.state
                 self.btn.configure(text="STOP")
+                self.count = 0
+                self.tmp = 0
             else:
                 self.state = False
                 self.btn.configure(text="START")
@@ -36,18 +44,18 @@ class App:
 
 
         self.delay = 15
-        self.tmp = 0
         self.update()
 
 
 
     def update(self):
         ret,frame = self.vid.get_frame()
-        self.img_rain = PIL.ImageTk.PhotoImage(PIL.Image.open('black.png').convert('RGB'))
+        self.img_rain = PIL.ImageTk.PhotoImage(PIL.Image.open('stop.png').convert('RGB'))
 
         if ret:
             gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
             if self.state is not False:
+                self.img_rain = PIL.ImageTk.PhotoImage(PIL.Image.open('norain.png').convert('RGB'))
                 #circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 50, param1=57, param2=40, minRadius=50, maxRadius=100000)
                 circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 50, param1=70, param2=75, minRadius=50, maxRadius=1000)
                 if circles is not None:
@@ -55,7 +63,19 @@ class App:
                     for i in circles[0,:]:
                         cv2.circle(frame,(i[0],i[1]),i[2],(0,255,255),8)
                         cv2.circle(frame,(i[0],i[1]),2,(255,0,0),3)
-                    self.img_rain = PIL.ImageTk.PhotoImage(PIL.Image.open('demo.png').convert('RGB'))
+                    self.img_rain = PIL.ImageTk.PhotoImage(PIL.Image.open('rain.png').convert('RGB'))
+                    self.count = 0
+                    if self.tmp == 0:
+                        print(1)
+                    self.tmp = 1
+                else:
+                    self.count += 1
+                    if self.count < 30 and self.tmp == 1:
+                        self.img_rain = PIL.ImageTk.PhotoImage(PIL.Image.open('rain.png').convert('RGB'))
+                    elif self.count >= 30:
+                        if self.tmp == 1:
+                            print(0)
+                        self.tmp = 0
                 
             self.img_stream = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame)) 
             self.stream.create_image(0, 0, image = self.img_stream, anchor = tkinter.NW)
@@ -81,7 +101,7 @@ class MyVideoCapture:
            self.vid.release()
 
 def main():
-    app = App(tkinter.Tk(), "リアルタイム降雨センサ","http://192.168.1.44:8080/?action=stream")
+    app = App(tkinter.Tk(), "","http://192.168.1.44:8080/?action=stream")
     app.window.mainloop()
 
 if __name__ == '__main__':
